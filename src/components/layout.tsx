69b690c608f0b682b9c7365e9f2a2a662782b671
yuichi04 <sparklingengineer.yuya04gen@gmail.com>
2023-01-22 18:56:41 +0900

    Add cssの型定義ファイルを追加

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { container, heading, navLinks, navLinkItem, navLinkText } from "./layout.module.css";
 
 type Props = {
   pageTitle: string;
@@ -8,19 +9,23 @@ type Props = {
 
 const Layout: React.FC<Props> = React.memo(({ pageTitle, children }) => {
   return (
-    <div>
+    <div className={container}>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
           </li>
         </ul>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
